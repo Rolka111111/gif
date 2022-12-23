74440624df353b113e54505fca059558188e1903
cobaan <115618285+remooot@users.noreply.github.com>
2022-12-24 00:54:56 +0700

    Create sendmenu.sh

---
+++ fodder/nginx/sendmenu.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# download script
+cd /usr/bin
+# menu
+wget -O add-ss "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/add-ss"
+wget -O add-ssh "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/add-ssh"
+wget -O add-tr "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/add-tr"
+wget -O add-vless "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/add-vless"
+wget -O add-ws "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/add-ws"
+wget -O cek-ss "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/cek-ss"
+wget -O cek-ssh "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/cek-ssh"
+wget -O cek-tr "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/cek-tr"
+wget -O cek-vless "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/cek-vless"
+wget -O cek-ws "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/cek-ws"
+wget -O del-ss "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/del-ss"
+wget -O del-ssh "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/del-ssh"
+wget -O del-tr "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/del-tr"
+wget -O del-vless "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/del-vless"
+wget -O del-ws "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/del-ws"
+wget -O get-backres "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/get-backres"
+wget -O get-domain "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/get-domain"
+wget -O ittil.bak "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/ittil.bak"
+wget -O logclean "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/logclean"
+wget -O menu "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/menu"
+wget -O portin "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/portin"
+wget -O renew-ss "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/renew-ss"
+wget -O renew-ssh "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/renew-ssh"
+wget -O renew-tr "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/renew-tr"
+wget -O renew-vless "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/renew-vless"
+wget -O renew-ws "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/renew-ws"
+wget -O run "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/run"
+wget -O seres "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/seres"
+wget -O shadowsocks "https://raw.githubusercontent.com/remooot/gif/main/fodder/shadowsocks"
+wget -O trojan "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/trojan"
+wget -O vless "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/vless"
+wget -O vmess "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/vmess"
+wget -O ssh "https://raw.githubusercontent.com/remooot/gif/main/fodder/nginx/ssh"
+
+
+chmod +x menu
+chmod +x ssh
+chmod +x shadowsocks
+chmod +x trojan
+chmod +x vmess
+chmod +x vless
+
+chmod +x add-ss
+chmod +x add-ssh
+chmod +x add-tr
+chmod +x add-vless
+chmod +x add-ws
+
+chmod +x cek-ss
+chmod +x cek-ssh
+chmod +x cek-tr
+chmod +x cek-vless
+chmod +x cek-ws
+
+chmod +x renew-ss
+chmod +x renew-ssh
+chmod +x renew-tr
+chmod +x renew-ws
+chmod +x renew-vless
+
+chmod +x del-ss
+chmod +x del-ssh
+chmod +x del-tr
+chmod +x del-vless
+chmod +x del-ws
+
+chmod +x xp
+chmod +x run
+chmod +x seres
+chmod +x get-backres
+chmod +x get-domain
+chmod +x portin
+
+cd
+# finihsing
+clear
