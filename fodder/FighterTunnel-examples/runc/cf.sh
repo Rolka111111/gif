9369631ee87d2f5f1c0febbc723f934c4a7e2a58
free untuk indonesia hebat <74614880+arismaramar@users.noreply.github.com>
2023-01-12 00:06:04 +0700

    Update cf.sh

--- fodder/FighterTunnel-examples/runc/cf.sh
+++ fodder/FighterTunnel-examples/runc/cf.sh
@@ -10,7 +10,7 @@ clear
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 apt install jq curl -y >/dev/null 2>&1
-DOMAIN=vpnmurah.me
+DOMAIN=remoot.my.id
 sub=$(</dev/urandom tr -dc a-x1-9 | head -c6 | tr -d '\r' | tr -d '\r\n')
 SUB_DOMAIN=${sub}.remoot.my.id
 CF_ID=arismar.amar@gmail.com
