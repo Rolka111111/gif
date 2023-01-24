36143388adad99073257422f2072789b8c16fe9e
free untuk indonesia hebat <74614880+arismaramar@users.noreply.github.com>
2023-01-25 03:38:25 +0700

    Update setupsi.sh

--- fodder/FighterTunnel-examples/runc/setupsi.sh
+++ fodder/FighterTunnel-examples/runc/setupsi.sh
@@ -5,24 +5,25 @@
 # Auther  : Geo Project
 # (C) Copyright 2022
 # =========================================
-clear
-DEFBOLD='\e[39;1m'
-RB='\e[31;1m'
-GB='\e[32;1m'
-YB='\e[33;1m'
-BB='\e[34;1m'
-MB='\e[35;1m'
-CB='\e[35;1m'
-WB='\e[37;1m'
-red='\e[1;31m'
-green='\e[0;32m'
-purple='\e[0;35m'
-orange='\e[0;33m'
-NC='\e[0m'
-Server_URL="raw.githubusercontent.com/arismaramar/gif/main/fodder/FighterTunnel-examples/runc"
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+#clear
+#DEFBOLD='\e[39;1m'
+#RB='\e[31;1m'
+#GB='\e[32;1m'
+##YB='\e[33;1m'
+#BB='\e[34;1m'
+#MB='\e[35;1m'
+#CB='\e[35;1m'
+#WB='\e[37;1m'
+#red='\e[1;31m'
+##green='\e[0;32m'
+#purple='\e[0;35m'
+#orange='\e[0;33m'
+#NC='\e[0m'
+#dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
+Server_URL="raw.githubusercontent.com/arismaramar/gif/main/fodder/FighterTunnel-examples/runc"
+
 #MYIP=$(wget -qO- ipv4.icanhazip.com);
 #echo "Checking VPS"
 #clear
