c035763584d97f441003e6f39e289a8025bd2731
free untuk indonesia hebat <74614880+arismaramar@users.noreply.github.com>
2023-01-26 00:28:43 +0700

    Update menu.sh

--- fodder/FighterTunnel-examples/runc/menu.sh
+++ fodder/FighterTunnel-examples/runc/menu.sh
@@ -9,6 +9,7 @@ P='\e[0;35m'
 B='\033[0;36m'
 G='\e[0;32m'
 N='\e[0m'
+export Server_URL="raw.githubusercontent.com/wunuit/Multiport/main"
 
 clear
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
@@ -27,6 +28,77 @@ tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+cek=$( curl -sS https://raw.githubusercontent.com/wunuit/IP/main/access | awk '{print $2}'  | grep $MYIP )
+Name=$(curl -sS https://raw.githubusercontent.com/wunuit/IP/main/access | grep $MYIP | awk '{print $4}')
+if [[ $cek = $MYIP ]]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo ""
+echo -e "Your IP is ${red}NOT REGISTER${NC} @ ${red}EXPIRED${NC}"
+echo ""
+echo -e "Please Contact ${green}Admin${NC}"
+echo -e "Telegram : t.me/wunuit"
+exit 0
+fi
+clear
+
+BURIQ() {
+    curl -sS https://raw.githubusercontent.com/wunuit/IP/main/access >/root/tmp
+    data=($(cat /root/tmp | grep -E "^### " | awk '{print $4}'))
+    for user in "${data[@]}"; do
+        exp=($(grep -E "^### $user" "/root/tmp" | awk '{print $3}'))
+        d1=($(date -d "$exp" +%s))
+        d2=($(date -d "$biji" +%s))
+        exp2=$(((d1 - d2) / 86400))
+        if [[ "$exp2" -le "0" ]]; then
+            echo $user >/etc/.$user.ini
+        else
+            rm -f /etc/.$user.ini >/dev/null 2>&1
+        fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+Name=$(curl -sS https://raw.githubusercontent.com/wunuit/IP/main/access | grep $MYIP | awk '{print $4}')
+echo $Name >/usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman() {
+    if [[ -f "/etc/.$Name.ini" ]]; then
+        CekTwo=$(cat /etc/.$Name.ini)
+        if [[ "$CekOne" = "$CekTwo" ]]; then
+            res="Expired"
+        fi
+    else
+        res="Permission Accepted..."
+    fi
+}
+
+PERMISSION() {
+    MYIP=$(wget -qO- ipv4.icanhazip.com);
+    IZIN=$(curl -sS https://raw.githubusercontent.com/wunuit/IP/main/access | awk '{print $3}' | grep $MYIP)
+    if [[ "$MYIP" = "$IZIN" ]]; then
+        Bloman
+    else
+        res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ "$res" = "Expired" ]; then
+Exp="\e[36mExpired\033[0m"
+rm -f /home/needupdate > /dev/null 2>&1
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/wunuit/IP/main/access | grep $MYIP | awk '{print $3}')
+fi
 
 # // Export Color & Information
 export RED='\033[0;31m'
@@ -38,7 +110,7 @@ export CYAN='\033[0;36m'
 export LIGHT='\033[0;37m'
 export NC='\033[0m'
 clear
-domain=$(cat /usr/local/etc/xray/domain)
+domain=$(cat /root/domain)
 
 # // nginx status
 nginx=$( systemctl status nginx | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
@@ -57,8 +129,28 @@ else
 fi
 
 # // script version
+myver="$(cat /home/ver)"
 
+# // script version check
+serverV=$( curl -sS https://${Server_URL}/version_check_v2)
 
+function updatews(){
+clear
+echo -e "[ ${GREEN}INFO${NC} ] Check for Script updates . . ."
+sleep 1
+cd
+wget -q -O /root/update-v2.sh "https://${Server_URL}/update-v2.sh" && chmod +x update-v2.sh && ./update-v2.sh
+sleep 1
+rm -f /root/update-v2.sh
+rm -f /home/ver
+version_check_v2=$( curl -sS https://${Server_URL}/version_check_v2)
+echo "$version_check_v2" >> /home/ver
+clear
+echo ""
+echo -e "[ ${GREEN}INFO${NC} ] Successfully Up To Date!"
+sleep 1
+echo ""
+read -n 1 -s -r -p "Press any key to continue..."
 menu
 }
 
@@ -89,7 +181,7 @@ echo -e " IP Address         :  $IPVPS"
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
 echo -e "     [ XRAY-CORE${NC} : ${status_xray} ]   [ NGINX${NC} : ${status_nginx} ]"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
-echo -e "      \033[1;37mMULTIPORT WEBSOCKET BY ANGGUN\033[0m"
+echo -e "      \033[1;37mMULTIPORT WEBSOCKET BY WUNUIT\033[0m"
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
 echo -e " Daily Data Usage   :  ${yell}$daily_usage${N}"
 echo -e " Monthly Data Usage :  ${yell}$monthly_usage${N}"
@@ -137,6 +229,8 @@ echo -e " \033[1;37mType [ x ] To Exit From Menu \033[0m"
 echo ""
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
 echo -e " Version       :\033[1;36m Multiport Websocket $myver\e[0m"
+echo -e " Client Name   : $Name"
+echo -e " Expiry Script : $Exp"
 echo -e " Status Script : ${G}Lifetime${NC}"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
 echo ""
