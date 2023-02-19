c6743f2a5cba7153f95f1e7ac76ce75338d98a94
free net <74614880+arismaramar@users.noreply.github.com>
2023-02-20 01:04:43 +0700

    Create senmenu.sh

---
+++ images/senmenu.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# // config Data
+echo -e "${GREEN}Config Data${NC}"
+wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/arismaramar/gif/main/images/add-ws.sh"
+wget -q -O /usr/bin/add-ssws "https://raw.githubusercontent.com/arismaramar/gif/main/images/add-ssws.sh"
+#wget -q -O /usr/bin/add-socks "https://raw.githubusercontent.com/arismaramar/gif/main/images/add-socks.sh"
+wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/arismaramar/gif/main/images/add-vless.sh"
+wget -q -O /usr/bin/add-tr "https://raw.githubusercontent.com/arismaramar/gif/main/images/add-tr.sh"
+wget -q -O /usr/bin/add-trgo "https://raw.githubusercontent.com/arismaramar/gif/main/images/add-trgo.sh"
+wget -q -O /usr/bin/autoreboot "https://raw.githubusercontent.com/arismaramar/gif/main/images/autoreboot.sh"
+wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/arismaramar/gif/main/images/restart.sh"
+wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/arismaramar/gif/main/images/tendang.sh"
+wget -q -O /usr/bin/clearlog "https://raw.githubusercontent.com/arismaramar/gif/main/images/clearlog.sh"
+wget -q -O /usr/bin/running "https://raw.githubusercontent.com/arismaramar/gif/main/images/running.sh"
+wget -q -O /usr/bin/cek-trafik "https://raw.githubusercontent.com/arismaramar/gif/main/images/cek-trafik.sh"
+wget -q -O /usr/bin/cek-speed "https://raw.githubusercontent.com/arismaramar/gif/main/images/speedtes_cli.py"
+wget -q -O /usr/bin/cek-bandwidth "https://raw.githubusercontent.com/arismaramar/gif/main/images/cek-bandwidth.sh"
+wget -q -O /usr/bin/cek-ram "https://raw.githubusercontent.com/arismaramar/gif/main/images/ram.sh"
+wget -q -O /usr/bin/limit-speed "https://raw.githubusercontent.com/arismaramar/gif/main/images/limit-speed.sh"
+wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/arismaramar/gif/main/images/menu-vless.sh"
+wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/arismaramar/gif/main/images/menu-vmess.sh"
+#wget -q -O /usr/bin/menu-socks "https://raw.githubusercontent.com/arismaramar/gif/main/images/menu-socks.sh"
+wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/arismaramar/gif/main/images/menu-ss.sh"
+wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/arismaramar/gif/main/images/menu-trojan.sh"
+#wget -q -O /usr/bin/menu-trgo "https://raw.githubusercontent.com/arismaramar/gif/main/images/menu-trgo.sh"
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/arismaramar/gif/main/images/menu-ssh.sh"
+#wget -q -O /usr/bin/menu-bckp "https://raw.githubusercontent.com/arismaramar/gif/main/images/menu-bckp-telegram.sh"
+wget -q -O /usr/bin/menu-bckp "https://raw.githubusercontent.com/arismaramar/gif/main/images/menu-bckp-github.sh"
+#wget -q -O /usr/bin/bckp "https://raw.githubusercontent.com/arismaramar/gif/main/images/bckpbot.sh"
+wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/arismaramar/gif/main/images/usernew.sh"
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/arismaramar/gif/main/images/menu.sh"
+wget -q -O /usr/bin/wbm "https://raw.githubusercontent.com/arismaramar/gif/main/images/webmin.sh"
+wget -q -O /usr/bin/changer "https://raw.githubusercontent.com/arismaramar/gif/main/images/changer.sh"
+wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/arismaramar/gif/main/images/xp.sh"
+#wget -q -O /usr/bin/update "https://raw.githubusercontent.com/arismaramar/gif/main/images/update.sh"
+chmod +x /usr/bin/add-ws
+chmod +x /usr/bin/add-ssws
+#chmod +x /usr/bin/add-socks
+chmod +x /usr/bin/add-vless
+chmod +x /usr/bin/add-tr
+#chmod +x /usr/bin/add-trgo
+chmod +x /usr/bin/usernew
+chmod +x /usr/bin/autoreboot
+chmod +x /usr/bin/restart
+chmod +x /usr/bin/tendang
+chmod +x /usr/bin/clearlog
+chmod +x /usr/bin/running
+chmod +x /usr/bin/cek-trafik
+chmod +x /usr/bin/cek-speed
+chmod +x /usr/bin/cek-bandwidth
+chmod +x /usr/bin/cek-ram
+chmod +x /usr/bin/limit-speed
+chmod +x /usr/bin/menu-vless
+chmod +x /usr/bin/menu-vmess
+chmod +x /usr/bin/menu-ss
+#chmod +x /usr/bin/menu-socks
+chmod +x /usr/bin/menu-trojan
+#chmod +x /usr/bin/menu-trgo
+chmod +x /usr/bin/menu-ssh
+chmod +x /usr/bin/menu-bckp
+chmod +x /usr/bin/menu
+#chmod +x /usr/bin/bckp
+chmod +x /usr/bin/wbm
+chmod +x /usr/bin/xp
+chmod +x /usr/bin/changer
+#chmod +x /usr/bin/update
