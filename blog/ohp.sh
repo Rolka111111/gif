efbaf296bad9808c257b4d7824de5761100253ce
free net <74614880+arismaramar@users.noreply.github.com>
2023-03-01 13:33:29 +0700

    Update ohp.sh

--- blog/ohp.sh
+++ blog/ohp.sh
@@ -21,7 +21,7 @@ Port_OHP='8787';
 
 #Installing ohp Server
 cd 
-wget -O /usr/local/bin/ohp "https://raw.githubusercontent.com/arismaramar/gif/main/images/ohp-mini"
+wget -O /usr/local/bin/ohp "https://raw.githubusercontent.com/arismaramar/gif/main/blog/ohp"
 chmod +x /usr/local/bin/ohp
 
 #Buat File OpenVPN TCP OHP
