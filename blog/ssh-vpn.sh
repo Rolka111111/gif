6f01003b277fb9408b499b786643a00de1864e49
free net <74614880+arismaramar@users.noreply.github.com>
2023-03-04 06:15:05 +0700

    Update ssh-vpn.sh

--- blog/ssh-vpn.sh
+++ blog/ssh-vpn.sh
@@ -90,7 +90,7 @@ gem install lolcat
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # set locale
-sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+#sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 
 
 install_ssl(){
@@ -184,11 +184,11 @@ echo "=== Install Dropbear ==="
 # install dropbear
 #apt -y install dropbear
 wget -q -O /etc/default/dropbear https://raw.githubusercontent.com/arismaramar/gif/main/fodder/FighterTunnel-examples/dropbear
-sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 50000 -p 109 -p 110 -p 69"/g' /etc/default/dropbear
-echo "/bin/false" >> /etc/shells
-echo "/usr/sbin/nologin" >> /etc/shells
+#sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+#sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
+#sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 50000 -p 109 -p 110 -p 69"/g' /etc/default/dropbear
+#echo "/bin/false" >> /etc/shells
+#echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/ssh restart
 /etc/init.d/dropbear restart
 
