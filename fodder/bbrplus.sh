cde0896e9ec31c71b274166cfdf6baad71c02721
free net <74614880+arismaramar@users.noreply.github.com>
2023-04-18 09:43:00 +0700

    Update bbrplus.sh

--- fodder/bbrplus.sh
+++ fodder/bbrplus.sh
@@ -1,57 +1,92 @@
-#!/bin/bash
-GIT_CMD="https://github.com/remooot/Linux-NetSpeed/raw/master/bbrplus/debian-ubuntu/x64/"
-startbbr() {
-    if [[ $(echo 4.14.129-bbrplus | awk -F'.' '{print $1}') -ge "5" ]]; then
-	echo "net.core.default_qdisc=cake" >> /etc/sysctl.conf
-	echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-        echo "net.ipv4.icmp_echo_ignore_all = 0" >>/etc/sysctl.conf
-        echo "net.ipv4.icmp_echo_ignore_broadcasts = 1" >>/etc/sysctl.conf
-        echo "net.ipv4.icmp_ignore_bogus_error_responses = 1" >>/etc/sysctl.conf
-        echo "fs.file-max = 1000000" >>/etc/sysctl.conf
-        echo "fs.inotify.max_user_instances = 8192" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_tw_reuse = 1" >>/etc/sysctl.conf
-        echo "net.ipv4.ip_local_port_range = 1024 65535" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_rmem = 16384 262144 8388608" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_wmem = 32768 524288 16777216" >>/etc/sysctl.conf
-        echo "net.core.somaxconn = 8192" >>/etc/sysctl.conf
-        echo "net.core.rmem_max = 16777216" >>/etc/sysctl.conf
-        echo "net.core.wmem_max = 16777216" >>/etc/sysctl.conf
-        echo "net.core.wmem_default = 2097152" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_max_tw_buckets = 5000" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_max_syn_backlog = 10240" >>/etc/sysctl.conf
-        echo "net.core.netdev_max_backlog = 10240" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_slow_start_after_idle = 0" >>/etc/sysctl.conf
-        echo "net.ipv4.ip_forward = 1" >>/etc/sysctl.conf
-    else
-	echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-	echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-        echo "net.ipv4.icmp_echo_ignore_all = 0" >>/etc/sysctl.conf
-        echo "net.ipv4.icmp_echo_ignore_broadcasts = 1" >>/etc/sysctl.conf
-        echo "net.ipv4.icmp_ignore_bogus_error_responses = 1" >>/etc/sysctl.conf
-        echo "fs.file-max = 1000000" >>/etc/sysctl.conf
-        echo "fs.inotify.max_user_instances = 8192" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_tw_reuse = 1" >>/etc/sysctl.conf
-        echo "net.ipv4.ip_local_port_range = 1024 65535" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_rmem = 16384 262144 8388608" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_wmem = 32768 524288 16777216" >>/etc/sysctl.conf
-        echo "net.core.somaxconn = 8192" >>/etc/sysctl.conf
-        echo "net.core.rmem_max = 16777216" >>/etc/sysctl.conf
-        echo "net.core.wmem_max = 16777216" >>/etc/sysctl.conf
-        echo "net.core.wmem_default = 2097152" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_max_tw_buckets = 5000" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_max_syn_backlog = 10240" >>/etc/sysctl.conf
-        echo "net.core.netdev_max_backlog = 10240" >>/etc/sysctl.conf
-        echo "net.ipv4.tcp_slow_start_after_idle = 0" >>/etc/sysctl.conf
-        echo "net.ipv4.ip_forward = 1" >>/etc/sysctl.conf
-    fi
+#!/usr/bin/env bash
+export PATH
+source /etc/os-release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+cat << EOF | sudo debconf-set-selections
+# Abort kernel # Abort kernel removal??
+linux-base	linux-base/removing-running-kernel	boolean	false
+EOF
+
+remove_all(){
+	sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+	sed -i '/fs.file-max/d' /etc/sysctl.conf
+	sed -i '/net.core.rmem_default/d' /etc/sysctl.conf
+	sed -i '/net.core.wmem_default/d' /etc/sysctl.conf
+	sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_tw_recycle/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_keepalive_time/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+	sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.route.gc_timeout/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_synack_retries/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_syn_retries/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_timestamps/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_max_orphans/d' /etc/sysctl.conf
+	sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
+	sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
+	sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_slow_start_after_idle/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
 }
-installbbrplus(){
-		mkdir bbrplus && cd bbrplus
-		wget -N --no-check-certificate ${GIT_CMD}linux-headers-4.14.129-bbrplus.deb >/dev/null 2>&1
-		wget -N --no-check-certificate ${GIT_CMD}linux-image-4.14.129-bbrplus.deb >/dev/null 2>&1
-		dpkg -i linux-headers-4.14.129-bbrplus.deb
-		dpkg -i linux-image-4.14.129-bbrplus.deb
-		cd .. && rm -rf bbrplus
+optimizing_system(){
+	sed -i '/fs.file-max/d' /etc/sysctl.conf
+	sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
+	sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
+	sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
+	sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
+	sed -i '/net.core.wmem_default/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
+	sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
+ 	sed -i '/net.ipv4.tcp_slow_start_after_idle/d' /etc/sysctl.conf
+	sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+	echo "fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 1024 65535
+net.ipv4.tcp_rmem = 16384 262144 8388608
+net.ipv4.tcp_wmem = 32768 524288 16777216
+net.core.somaxconn = 8192
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.core.wmem_default = 2097152
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_max_syn_backlog = 10240
+net.core.netdev_max_backlog = 10240
+net.ipv4.tcp_slow_start_after_idle = 0
+# forward ipv4
+net.ipv4.ip_forward = 1">>/etc/sysctl.conf
+	sysctl -p
+	echo "*               soft    nofile           1000000
+*               hard    nofile          1000000">/etc/security/limits.conf
+	echo "ulimit -SHn 1000000">>/etc/profile
+} 
+startbbr(){
+	remove_all
+	if [[ `echo ${kernel_version} | awk -F'.' '{print $1}'` -ge "5" ]]; then
+		echo "net.core.default_qdisc=cake" >> /etc/sysctl.conf
+		echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+	else
+		echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+		echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+	fi
+	sysctl -p
 }
-startbbr
-installbbrplus
+apt-get --fix-broken install -y && apt-get autoremove -y
+startbbr   
