05bd18382463b32cae6ffb7d9fbb2b2fc82756f6
free net <74614880+arismaramar@users.noreply.github.com>
2023-04-18 12:58:49 +0700

    Update sendmenu.sh

--- XrayDNS/sendmenu.sh
+++ XrayDNS/sendmenu.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 GITHUB_CMD="https://github.com/arismaramar/gif/raw/"
-wget -O /usr/bin/menu "${GITHUB_CMD}main/XrayDNS/menu" >/dev/null 2>&1
-wget -O /usr/bin/run "${GITHUB_CMD}main/XrayDNS/run" >/dev/null 2>&1
-wget -O /usr/bin/seres "${GITHUB_CMD}main/XrayDNS/seres" >/dev/null 2>&1
-wget -O /usr/bin/shadowsocks "${GITHUB_CMD}main/XrayDNS/shadowsocks" >/dev/null 2>&1
-wget -O /usr/bin/trojan "${GITHUB_CMD}main/XrayDNS/trojan" >/dev/null 2>&1
-wget -O /usr/bin/vless "${GITHUB_CMD}main/XrayDNS/vless" >/dev/null 2>&1
-wget -O /usr/bin/vmess "${GITHUB_CMD}main/XrayDNS/vmess" >/dev/null 2>&1
-wget -O /usr/bin/ssh "${GITHUB_CMD}main/XrayDNS/ssh" >/dev/null 2>&1
-wget -O /usr/bin/xp "${GITHUB_CMD}main/XrayDNS/xp" >/dev/null 2>&1
-wget -O /usr/bin/bkp "${GITHUB_CMD}main/XrayDNS/bkp" >/dev/null 2>&1
-wget -O /usr/bin/net "${GITHUB_CMD}main/XrayDNS/net" >/dev/null 2>&1
-wget -O /usr/bin/gotop "${GITHUB_CMD}main/XrayDNS/gotop" >/dev/null 2>&1
-wget -O /usr/bin/logclean "${GITHUB_CMD}main/XrayDNS/logclean" >/dev/null 2>&1
-wget -O /usr/bin/portin "${GITHUB_CMD}main/XrayDNS/portin" >/dev/null 2>&1
-wget -O /usr/bin/settings "${GITHUB_CMD}main/XrayDNS/settings" >/dev/null 2>&1
-wget -O /usr/bin/restart "${GITHUB_CMD}main/XrayDNS/restart" >/dev/null 2>&1
-wget -O /usr/bin/speedtest "${GITHUB_CMD}main/XrayDNS/speedtest" >/dev/null 2>&1
-wget -O /usr/bin/cloudfront "${GITHUB_CMD}main/XrayDNS/cloudfront" >/dev/null 2>&1
-wget -O /usr/bin/about "${GITHUB_CMD}main/XrayDNS/about" >/dev/null 2>&1
-wget -O /usr/bin/domain "${GITHUB_CMD}main/XrayDNS/domain" >/dev/null 2>&1
+wget -q -O /usr/bin/menu "${GITHUB_CMD}main/XrayDNS/menu" >/dev/null 2>&1
+wget -q -O /usr/bin/run "${GITHUB_CMD}main/XrayDNS/run" >/dev/null 2>&1
+wget -q -O /usr/bin/seres "${GITHUB_CMD}main/XrayDNS/seres" >/dev/null 2>&1
+wget -q -O /usr/bin/shadowsocks "${GITHUB_CMD}main/XrayDNS/shadowsocks" >/dev/null 2>&1
+wget -q -O /usr/bin/trojan "${GITHUB_CMD}main/XrayDNS/trojan" >/dev/null 2>&1
+wget -q -O /usr/bin/vless "${GITHUB_CMD}main/XrayDNS/vless" >/dev/null 2>&1
+wget -q -O /usr/bin/vmess "${GITHUB_CMD}main/XrayDNS/vmess" >/dev/null 2>&1
+wget -q -O /usr/bin/ssh "${GITHUB_CMD}main/XrayDNS/ssh" >/dev/null 2>&1
+wget -q -O /usr/bin/xp "${GITHUB_CMD}main/XrayDNS/xp" >/dev/null 2>&1
+wget -q -O /usr/bin/bkp "${GITHUB_CMD}main/XrayDNS/bkp" >/dev/null 2>&1
+wget -q -O /usr/bin/net "${GITHUB_CMD}main/XrayDNS/net" >/dev/null 2>&1
+wget -q -O /usr/bin/gotop "${GITHUB_CMD}main/XrayDNS/gotop" >/dev/null 2>&1
+wget -q -O /usr/bin/logclean "${GITHUB_CMD}main/XrayDNS/logclean" >/dev/null 2>&1
+wget -q -O /usr/bin/portin "${GITHUB_CMD}main/XrayDNS/portin" >/dev/null 2>&1
+wget -q -O /usr/bin/settings "${GITHUB_CMD}main/XrayDNS/settings" >/dev/null 2>&1
+wget -q -O /usr/bin/restart "${GITHUB_CMD}main/XrayDNS/restart" >/dev/null 2>&1
+wget -q -O /usr/bin/speedtest "${GITHUB_CMD}main/XrayDNS/speedtest" >/dev/null 2>&1
+wget -q -O /usr/bin/cloudfront "${GITHUB_CMD}main/XrayDNS/cloudfront" >/dev/null 2>&1
+wget -q -O /usr/bin/about "${GITHUB_CMD}main/XrayDNS/about" >/dev/null 2>&1
+wget -q -O /usr/bin/domain "${GITHUB_CMD}main/XrayDNS/domain" >/dev/null 2>&1
 
 chmod +x /usr/bin/menu
 chmod +x /usr/bin/ssh
