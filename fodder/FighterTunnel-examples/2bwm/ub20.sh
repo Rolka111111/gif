0a05e05f7aae89a4d359e3c2b0a4e4f23afb9fd3
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-06-05 23:47:15 +0300

    Update ub20.sh

--- fodder/FighterTunnel-examples/2bwm/ub20.sh
+++ fodder/FighterTunnel-examples/2bwm/ub20.sh
@@ -27,7 +27,6 @@ red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 TIMES="10"
 NAMES=$(whoami)
 IMP="wget -q -O"
-CHATID="1423578532"
 LOCAL_DATE="/usr/bin/"
 MYIP=$(wget -qO- ipinfo.io/ip)
 CITY=$(curl -s ipinfo.io/city)
